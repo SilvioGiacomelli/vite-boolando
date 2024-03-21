0d64c949cd3fa11b0fc814d1154ab71cc8e30d45
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-03-21 17:32:00 +0100

    [FINITO]

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,32 +4,60 @@ export default {}
 
 <template>
 <footer>
-  <div class="l-footer">
-    <p>Boolando s.r.l</p>
-    <div>
-      <ul>
-        <li>Informazioni legali</li>
-        <li>Informativa sulla privacy</li>
-        <li>Diritto di recesso</li>
-      </ul>
+  <div class="intro-foot">
+    <div class="l-footer">
+      <p>Boolando s.r.l</p>
+      <div>
+        <ul>
+          <li>Informazioni legali</li>
+          <li>Informativa sulla privacy</li>
+          <li>Diritto di recesso</li>
+        </ul>
+      </div>
     </div>
-  </div>
-  <div class="r-footer">
-    <p>Trovaci anche su</p>
-    <div>
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+    <div class="r-footer">
+      <p>Trovaci anche su</p>
+      <div>
+        <ul>
+          <li><a href="#"><i class="fa-brands fa-x-twitter"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-square-facebook"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-square-instagram"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-square-pinterest"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-square-youtube"></i></a></li>
+        </ul>
+      </div>
     </div>
-  </div>
+</div>
 </footer>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 @use '../assets/scss/main.scss' as *;
+
+.intro-foot{
+  max-width: 1200px;
+  margin: 0 auto;
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+}
+p, li{
+  color: white;
+  list-style: none;
+} 
+
+ul{
+  display: flex;
+  padding: 10px 10px 0 0;
+}
+
+i{
+  padding-right: 10px;
+}
+
+footer{
+  background-color: $bg-black;
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,4 +11,7 @@ export default {}
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 @use '../assets/scss/main.scss' as *;
+h2{
+  text-align: center;
+}
 </style>
