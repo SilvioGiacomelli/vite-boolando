ed109a849d484336673e170555b320074455bbd9
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-03-22 16:46:01 +0100

    [comparse 6 immagini]

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,33 @@
 <script>
-export default {}
+import Prodotto from './partials/Prodotto.vue';
+export default {
+  components:{
+    Prodotto
+  }
+}
 </script>
 
 <template>
-  <div>
-    <h2>MAIN</h2>
-  </div>
+  <main>
+    <div class="container">
+      <Prodotto />
+      <Prodotto />
+      <Prodotto />
+      <Prodotto />
+      <Prodotto />
+      <Prodotto />
+    </div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 @use '../assets/scss/main.scss' as *;
-h2{
-  text-align: center;
+
+.container{
+  max-width: 1200px;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/partials/Prodotto.vue
@@ -0,0 +1,16 @@
+<script>
+  export default {      
+  }
+</script>
+<template>
+  <div class="card">
+    <img src="../../assets/img/1.webp" alt="">
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.card{
+  width: calc(100%/3);
+}
+</style>
